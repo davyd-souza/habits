bebe66c45250051eab64fd82bfc11ae5d8a7de31
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-01-28 18:12:42 -0300

    feat(server): initial setup of API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,22 @@
-console.log("test")
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async () => {
+  const habit = await prisma.habit.findMany()
+
+  return habit
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server running!')
+  })
