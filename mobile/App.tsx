9623ac30d7e3e1b9d092352b439a99593c9d056b
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-01-28 23:32:17 -0300

    feat(mobile): implement inter font

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,29 @@
 // COMPONENT
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { Loading } from './src/components/Loading'
+
+// STYLE
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World!</Text>
@@ -14,7 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#121214',
+    backgroundColor: '#09090a',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -22,5 +43,6 @@ const styles = StyleSheet.create({
     color: '#fafafa',
     fontSize: 32,
     fontWeight: '500',
+    fontFamily: 'Inter_600SemiBold',
   },
 })
