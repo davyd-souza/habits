253f69a20f977c10ad8a21cab5003a486cd25258
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-01-28 23:31:47 -0300

    feat(mobile): create loading

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,17 @@
+// COMPONENT
+import { ActivityIndicator, View } from 'react-native'
+
+export function Loading() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#09090a',
+      }}
+    >
+      <ActivityIndicator color='#7c3aed' />
+    </View>
+  )
+}
