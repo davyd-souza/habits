ef035f8a7701131f7ad13a8ad8b64f2a14f8fb25
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-03 02:41:37 -0300

    feat(web): implement habit day square

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,13 @@
+// DEPENDENCY
+import React from 'react'
+
+// TYPE
+// interface HabitDayProps {
+//   completed: number
+// }
+
+export function HabitDay(): JSX.Element {
+  return (
+    <div className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg'></div>
+  )
+}
