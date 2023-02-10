ede65f21d8b31f748616eb73a860e2f95613d8da
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-09 23:36:41 -0300

    feat(server): add route to toggle habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -80,4 +80,55 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits,
     }
   })
+
+  // complete and uncomplete a habit
+  app.patch('/habits/:id/toggle', async (req) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleHabitParams.parse(req.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      })
+    }
+
+    // Searching to see if there's a habit completed on day
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    })
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      })
+    } else {
+      // Complete habit on day
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      })
+    }
+  })
 }
