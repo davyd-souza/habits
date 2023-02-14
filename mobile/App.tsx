1eb2d9dec33294051bbf35ab615cab953d2a968f
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-13 22:01:49 -0300

    feat(mobile): setup routing

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import './src/lib/dayjs'
 // COMPONENT
 import { StatusBar } from 'react-native'
 import { Loading } from './src/components/Loading'
-import { Home } from './src/screens/Home'
+import { Routes } from './src/routes'
 
 // STYLE
 import {
@@ -29,7 +29,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,15 @@
+// DEPENDENCY
+import { View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+
+import { AppRoutes } from './app.routes'
+
+export function Routes(): JSX.Element {
+  return (
+    <View className='flex-1 bg-background'>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
+  )
+}
