36ea0e22ad4736c42693cba6c3cbab480f8394da
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-13 22:02:37 -0300

    style(mobile): organize imports

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,10 +1,12 @@
 // COMPONENT
 import { View, TouchableOpacity, Text } from 'react-native'
-import Logo from '../assets/logo.svg'
-import { MaterialIcons } from '@expo/vector-icons'
 
 // STYLE
 import colors from 'tailwindcss/colors'
+import { MaterialIcons } from '@expo/vector-icons'
+
+// ASSET
+import Logo from '../assets/logo.svg'
 
 export function Header(): JSX.Element {
   return (
