95ff53d57fcd25cc7fbfaf27da942c8bd9e64c1f
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-13 22:01:16 -0300

    feat(mobile): create initial habit screen

---
+++ mobile/src/screens/Habit.tsx
@@ -0,0 +1,10 @@
+// DEPENDENCY
+import { Text, View } from 'react-native'
+
+export function Habit(): JSX.Element {
+  return (
+    <View className='flex-1 bg-background px-8 pt-16'>
+      <Text>Habit</Text>
+    </View>
+  )
+}
