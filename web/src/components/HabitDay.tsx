1b789987483dd542e42df9e888fbcf96796580b8
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-18 00:02:37 -0300

    feat(web): dynamically load habits and progress

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,23 +1,39 @@
 // DEPENDENCY
-import React from 'react'
+import { useState } from 'react'
 import clsx from 'clsx'
+import dayjs from 'dayjs'
 
 // COMPONENT
 import * as Popover from '@radix-ui/react-popover'
-import * as Checkbox from '@radix-ui/react-checkbox'
 import { ProgressBar } from './ProgressBar'
-
-// STYLE
-import { Check } from 'phosphor-react'
+import { HabitsList } from './HabitsList'
 
 // TYPE
 interface HabitDayProps {
-  completed: number
-  amount: number
+  date: Date
+  defaultCompleted?: number
+  amount?: number
 }
 
-export function HabitDay({ completed, amount }: HabitDayProps): JSX.Element {
-  const completedPercentage = Math.round((completed / amount) * 100)
+export function HabitDay({
+  date,
+  defaultCompleted = 0,
+  amount = 0,
+}: HabitDayProps): JSX.Element {
+  const [completed, setCompleted] = useState(defaultCompleted)
+
+  const completedPercentage =
+    amount > 0 ? Math.round((completed / amount) * 100) : 0
+
+  const today = dayjs().startOf('day').toDate()
+  const isCurrentDay = dayjs(date).isSame(today)
+
+  const dayAndMonth = dayjs(date).format('DD/MM')
+  const dayOfWeek = dayjs(date).format('dddd')
+
+  function handleCompletedChanged(completed: number) {
+    setCompleted(completed)
+  }
 
   return (
     <Popover.Root>
@@ -33,31 +49,19 @@ export function HabitDay({ completed, amount }: HabitDayProps): JSX.Element {
           'bg-violet-600 border-violet-500':
             completedPercentage >= 60 && completedPercentage < 80,
           'bg-violet-500 border-violet-400': completedPercentage >= 80,
+          'border-white border-2': isCurrentDay,
         })}
       />
 
       <Popover.Portal>
         <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 grid'>
-          <span className='font-semibold text-zinc-400'>sexta-feira</span>
+          <span className='font-semibold text-zinc-400'>{dayOfWeek}</span>
           <span className='mt-1 font-extrabold leading-tight text-3xl'>
-            10/02
+            {dayAndMonth}
           </span>
 
           <ProgressBar progress={completedPercentage} />
-
-          <div className='mt-6 flex flex-col gap-3'>
-            <Checkbox.Root className='flex items-center gap-3 group'>
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-                <Checkbox.Indicator>
-                  <Check size={20} className='text-white' />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList date={date} onCompletedChanged={handleCompletedChanged} />
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
       </Popover.Portal>
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,8 +1,23 @@
+// DEPENDENCY
+import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+
 // COMPONENT
 import { HabitDay } from './HabitDay'
 
 // UTIL
 import { generateDatesFromYarBeginning } from '../utils/generate-dates-from-year-beginning'
+
+// LIB
+import { api } from '../lib/axios'
+
+// TYPE
+type Summary = Array<{
+  id: string
+  date: string
+  amount: number
+  completed: number
+}>
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
@@ -12,6 +27,12 @@ const minimumSummaryDatesSize = 18 * 7 // 18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable(): JSX.Element {
+  const [summary, setSummary] = useState<Summary>([])
+
+  useEffect(() => {
+    api.get('summary').then((res) => setSummary(res.data))
+  }, [])
+
   return (
     <div className='w-full flex'>
       <div className='grid grid-rows-7 grid-flow-row gap-3'>
@@ -28,13 +49,21 @@ export function SummaryTable(): JSX.Element {
       </div>
 
       <div className='grid grid-rows-7 grid-flow-col gap-3'>
-        {summaryDates.map((date) => (
-          <HabitDay
-            key={date.toString()}
-            amount={5}
-            completed={Math.round(Math.random() * 5)}
-          />
-        ))}
+        {summary.length > 0 &&
+          summaryDates.map((date) => {
+            const dayInSummary = summary.find((day) =>
+              dayjs(date).isSame(day.date, 'day')
+            )
+
+            return (
+              <HabitDay
+                key={date.toString()}
+                date={date}
+                amount={dayInSummary?.amount}
+                defaultCompleted={dayInSummary?.completed}
+              />
+            )
+          })}
 
         {amountOfDaysToFill > 0 &&
           Array.from({ length: amountOfDaysToFill }).map((_, i) => (
