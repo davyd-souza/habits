d8b611a25f55511c2eb10a650ac65c322e2a78ca
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-19 19:32:55 -0300

    feat(mobile): add option to refetch api on hool

--- mobile/src/hooks/useFetch.ts
+++ mobile/src/hooks/useFetch.ts
@@ -1,7 +1,6 @@
 // DEPENDENCY
 import { AxiosRequestConfig } from 'axios'
 import { useState, useEffect } from 'react'
-import { Alert } from 'react-native'
 
 // LIB
 import { api } from '../lib/axios'
@@ -10,9 +9,10 @@ export function useFetch<T = unknown>(
   url: string,
   extraOptions?: AxiosRequestConfig
 ) {
-  const [response, setResponse] = useState<T | null>()
+  const [response, setResponse] = useState<T | null>(null)
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState<Error | null>(null)
+  const [shouldRefetch, refetch] = useState({})
 
   useEffect(() => {
     api
@@ -20,7 +20,7 @@ export function useFetch<T = unknown>(
       .then((res) => setResponse(res.data))
       .catch((err) => setError(err))
       .finally(() => setIsLoading(false))
-  }, [])
+  }, [shouldRefetch])
 
-  return { response, error, isLoading }
+  return { response, error, isLoading, refetch: () => refetch({}) }
 }
